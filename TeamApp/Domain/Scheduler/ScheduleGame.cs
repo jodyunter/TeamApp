bfb6c7713732a1ca2ed577dc47368881f72eb004
Jody Unterschutz <jody_unterschutz@transcanada.com>
2018-09-11 08:54:27 -0600

    m

---
+++ TeamApp/Domain/Scheduler/ScheduleGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamApp.Domain.Competition.Season;
+
+namespace TeamApp.Domain.Scheduler
+{
+    public class ScheduleGame
+    {        
+        League League { get; set; }
+        int GameNumber { get; set; }
+        int Day { get; set; }
+        int Year { get; set; }
+        Team Team { get; set; }  //this unifies it among all competitions and history
+        Season Season { get; set; } //if null it is exhibition game
+        //future playoff option too
+        int HomeScore { get; set; }
+        int AwayScore { get; set; }
+        bool Complete { get; set; }
+        bool CanTie { get; set; }
+        int MaxOverTimePeriods { get; set; }
+    }
+}
---
+++ TeamApp/Domain/Scheduler/Scheduler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamApp.Domain.Competition.Season;
+
+namespace TeamApp.Domain.Scheduler
+{
+    public class Scheduler
+    {
+        public static List<ScheduleGame> CreateGames(League league, int lastGameNumber, Season season, int startDay, List<Team> HomeTeams, List<Team> AwayTeams)
+        {
+
+            
+            
+        }
+    }
+}
