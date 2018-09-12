36461b31d9d9b4b77e70c69e4f5b291751047f17
Jody Unterschutz <jody_unterschutz@transcanada.com>
2018-09-12 10:17:14 -0600

    m

---
+++ TeamApp/Domain/Scheduler/Schedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamApp.Domain.Scheduler
+{
+    public class Schedule
+    {
+        public Dictionary<int, ScheduleDay> Days { get; set; }
+
+
+    }
+}
