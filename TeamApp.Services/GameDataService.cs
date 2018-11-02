feba1b79a5dc7789457331a3dedcc0eec7b74e63
Jody Unterschutz <jody_unterschutz@transcanada.com>
2018-11-02 09:47:16 -0600

    m

--- TeamApp.Services/GameDataService.cs
+++ TeamApp.Services/GameDataService.cs
@@ -5,76 +5,28 @@ using TeamApp.Domain;
 using TeamApp.Domain.Competitions;
 using TeamApp.Domain.Repositories;
 using TeamApp.Domain.Schedules;
+using TeamApp.Services.ViewModels.Views;
 
 namespace TeamApp.Services
 {
     public class GameDataService
     {
-        private IGameDataRepository GameDataRepository;        
-        private ILeagueRepository LeagueRepository;
-        private IScheduleRepository ScheduleRepository;
 
-        public GameDataService(IGameDataRepository gameDataRepository, ILeagueRepository leagueRepository, IScheduleRepository scheduleRepository)
+        public GameDataService()
         {
-            GameDataRepository = gameDataRepository;
-            LeagueRepository = leagueRepository;
         }
 
-        public GameData GetGameData()
+        public GameSummary GetGameSummary()
         {
-            return GameDataRepository.First();
-        }
-
-        public List<League> GetActiveLeagues()
-        {
-            var currentYear = GameDataRepository.First().CurrentYear;
-
-            return LeagueRepository.Where(l => l.FirstYear != null && l.FirstYear <= currentYear && (l.LastYear == null || l.LastYear >= currentYear)).ToList();
-        }
-
-        public List<League> GetAllLeagues()
-        {
-            return LeagueRepository.ToList();
-        }
-
-        public void StartNextCompetition()
-        {
-            var data = GameDataRepository.First();
-
-            //var competitions = null;
-        }
-
-        public int GetCurrentYear()
-        {
-            return GameDataRepository.First().CurrentYear;
-        }
-
-        public int GetCurrentDay()
-        {
-            return GameDataRepository.First().CurrentDay;
-        }
-
-        public List<ScheduleGame> GetTodaysGames()
-        {
-            return GetGamesOnDay(GetCurrentYear(), GetCurrentDay());
-        }
-
-        public List<ScheduleGame> GetGamesOnDay(int year, int dayNumber)
-        {
-            return ScheduleRepository.Where(s => s.Year == year && s.Day == dayNumber).ToList();
-        }
-
-        public Dictionary<int, List<ScheduleGame>> GetGamesForDayRange(int year, int firstDay, int lastDay)
-        {
-            var results = new Dictionary<int, List<ScheduleGame>>();
-
-            for (int i = firstDay; i <= lastDay; i++)
+            return new GameSummary()
             {
-                results.Add(i, GetGamesOnDay(year, i));
+                CurrentDay = 125,
+                CurrentYear = 5
+            
             }
 
-            return results;
         }
+
  
     }
 }
---
+++ TeamApp.Services/ViewModels/Views/GameSummary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamApp.Services.ViewModels.Views
+{
+    public class GameSummary
+    {
+        public int CurrentYear { get; set; }
+        public int CurrentDay { get; set; }
+    }
+}
