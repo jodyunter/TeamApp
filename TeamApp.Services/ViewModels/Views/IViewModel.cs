0200865fac297fe592ca6d8b44b7056bfcb6b939
Jody Unterschutz <jody_unterschutz@transcanada.com>
2018-11-05 08:43:36 -0700

    m

---
+++ TeamApp.Services/ViewModels/Views/IViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamApp.Services.ViewModels.Views
+{
+    public interface IViewModel
+    {
+        IViewModel GetViewModel(object obj);
+
+    }
+}
