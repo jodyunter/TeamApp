5d775bc56657aa8377f17443dabd59b7a180932c
Jody Unterschutz <jody_unterschutz@transcanada.com>
2018-11-08 09:01:03 -0700

    m

---
+++ TeamApp.Services/TeamService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamApp.Domain.Repositories;
+
+namespace TeamApp.Services
+{
+    public class TeamService
+    {
+        ITeamRepository teamRepository;
+
+        public TeamService()
+        {
+
+        }
+
+        public TeamView
+
+    }
+}
---
+++ TeamApp.Services/ViewModels/Views/TeamViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamApp.Services.ViewModels.Views
+{
+    public class TeamViewModel
+    {
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public string ShortName { get; set; }
+        public int Skill { get; set; }
+        public string Owner { get; set; }
+        public int? FirstYear { get; set; }
+        public int? LastYear { get; set; }
+        public bool Active { get; set; }
+    }
+}
