248361c52f36cf17ef127e26e3b697d9035f0536
Jody Unterschutz <jody_unterschutz@transcanada.com>
2018-12-20 10:25:40 -0700

    m

---
+++ TeamApp.Services/ILeagueService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamApp.Services
+{
+    public interface ILeagueService
+    {
+        IEnumerable<CompetitionConfigViewModel> GetCompetitionConfigs(long leagueId);
+        IEnumerable<SeasonCompetitionConfigViewModel> GetSeasonCompetitionConfig(long competitiongConfigId);
+    }
+}
---
+++ TeamApp.Services/ViewModels/Views/CompetitionConfig/CompetitionConfigViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamApp.Services.ViewModels.Views.CompetitionConfig
+{
+    public class CompetitionConfigViewModel
+    {
+    }
+}
