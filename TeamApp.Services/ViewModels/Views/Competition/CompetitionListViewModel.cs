fa629012b178ce5b668a9b9b203f84fcde0928ef
Jody Unterschutz <jody_unterschutz@transcanada.com>
2019-01-18 12:55:14 -0700

    m

---
+++ TeamApp.Services/ViewModels/Views/Competition/CompetitionListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamApp.Services.ViewModels.Views.Season
+{
+    public class CompetitionListViewModel
+    {
+    }
+}
---
+++ TeamApp.Services/ViewModels/Views/Competition/CompetitionSimpleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamApp.Services.ViewModels.Views.Season
+{
+    public class CompetitionSimpleViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public bool Complete { get; set; }
+        public string LeagueName { get; set; }
+    }
+}
