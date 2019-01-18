99c305a8c8806c78bcdb58a92ece7579eda83330
Jody Unterschutz <jody_unterschutz@transcanada.com>
2019-01-18 15:06:33 -0700

    m

---
+++ TeamApp.Data/Repositories/SeasonRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamApp.Domain.Competitions.Seasons;
+using TeamApp.Domain.Repositories;
+
+namespace TeamApp.Data.Repositories
+{
+    public class SeasonRepository:DataRepository<Season> : ISeasonRepository
+    {
+        public SeasonRepository(IRepository<Season> repo) : base(repo) { }
+
+        public Season GetByLeagueAndYear(int leagueId, int year)
+        {
+            return this.Where
+        }
+    }
+}
---
+++ TeamApp/Domain/Repositories/ISeasonRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamApp.Domain.Competitions.Seasons;
+
+namespace TeamApp.Domain.Repositories
+{
+    public interface ISeasonRepository:IRepository<Season>
+    {
+        Season GetByLeagueAndYear(int leagueId, int year);
+    }
+}
