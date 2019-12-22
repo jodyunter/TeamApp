2e7f7f619613dbbc62a8ccdbb07cba80a11ea51c
Jody Unterschutz <jody.u01@gmail.com>
2019-12-22 11:36:12 -0700

    m

---
+++ TeamApp.Interface.Console/Views/ScheduleDayView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TeamApp.Console.Views
+{
+    public class ScheduleDayView
+    {
+        public int CurrentYear { get; set; }
+        public int CurrentDay { get; set; }
+        public IList<ScheduleGameDayView> Games { get; set; }
+        public string GetView()
+        {
+            var result = "Year: " + CurrentYear;
+            result += "\nDay: " + CurrentDay;
+
+            Games.ToList().ForEach(game =>
+            {
+                game.GetView();
+            });
+
+            return result;
+        }
+    }
+}
---
+++ TeamApp.Interface.Console/Views/ScheduleGameDayView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamApp.Domain.Games;
+using TeamApp.Domain.Schedules;
+
+namespace TeamApp.Console.Views
+{
+    public class ScheduleGameDayView
+    {
+        public ScheduleGame Game { get; set; }
+
+        public ScheduleGameDayView(ScheduleGame game)
+        {
+            Game = game;
+        }
+
+        public string GetView()
+        {
+            var formatter = "{0,15}: {1,2}{5,2}{2,2} :{3,-15} {4}";
+            var result = "";
+
+            if (Game.Complete) result += " Final";
+            else result += " " + Game.CurrentPeriod.DisplayWithSuffix();
+
+            return string.Format(formatter, Game.Home.Name, Game.HomeScore, Game.AwayScore, Game.Away.Name, result, "-");
+        }
+    }
+}
