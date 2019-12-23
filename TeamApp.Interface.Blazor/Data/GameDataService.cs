9bc3914db7d435628996322fb3b7749d53d2194d
jodyu <jody_unterschutz@tcenergy.com>
2019-12-23 10:10:50 -0700

    More hcanges that don't really work, using Radzen

---
+++ TeamApp.Interface.Blazor/Data/GameDataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamApp.ViewModels.Views;
+
+namespace TeamApp.Interface.Blazor.Data
+{
+    public class GameDataService
+    {
+        public Task<GameSummary> GetGameSummary()
+        {
+            var service = new GameDataService();
+
+            return service.GetGameSummary();
+        }
+        
+    }
+}
