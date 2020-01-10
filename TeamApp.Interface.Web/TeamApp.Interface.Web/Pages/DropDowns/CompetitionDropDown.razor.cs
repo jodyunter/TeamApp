4ab92dfedb84c10a075bb55f9d515a0003760a2a
jodyu <jody_unterschutz@tcenergy.com>
2020-01-10 14:27:25 -0700

    Done

---
+++ TeamApp.Interface.Web/TeamApp.Interface.Web/Pages/DropDowns/CompetitionDropDown.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamApp.Interface.Web.States;
+using TeamApp.Services;
+using TeamApp.ViewModels.Views;
+using TeamApp.ViewModels.Views.Competition;
+
+namespace TeamApp.Interface.Web.Pages.DropDowns
+{
+    public partial class YearDropDown:ComponentBase
+    {
+        [Inject] ICompetitionService CompetitionService { get; set; }
+        [Inject] AppState AppState { get; set; }
+        [Parameter] public IList<CompetitionViewModel> Leagues { get; set; }
+        
+        public LeagueViewModel SelectedLeague { get; set; }        
+
+        public string DropDownHeaderValue
+        {
+            get
+            {
+                var result = "League";
+                return SelectedLeague == null ? result : result + " - " + SelectedLeague.Name;
+            }
+        }
+        protected override void OnInitialized()
+        {
+            if (Leagues == null)
+            {
+                Leagues = LeagueService.GetAll().ToList();
+            }
+
+
+        }
+
+        protected void ChooseLeague(LeagueViewModel chosenLeague)
+        {
+            SelectedLeague = chosenLeague;
+            AppState.SetLeague(SelectedLeague);
+        }
+    }
+}
